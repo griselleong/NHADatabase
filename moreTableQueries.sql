183d713f1462e7b6f28c659ece58b3a1019562b0
griselleong <griselleong@gmail.com>
2021-05-06 22:18:31 -0400

    Create moreTableQueries.sql

---
+++ moreTableQueries.sql
@@ -0,0 +1,12 @@
+SELECT school_year, count(*) 
+FROM StudentCourseHistory sch
+INNER JOIN Student s ON s.id = sch.student_id
+INNER JOIN Course c ON c.id = sch.course_id
+GROUP BY school_year
+
+SELECT school_year, semester, count(*) 
+FROM StudentCourseHistory sch
+INNER JOIN Student s ON s.id = sch.student_id
+INNER JOIN Course c ON c.id = sch.course_id
+GROUP BY school_year, semester
+
