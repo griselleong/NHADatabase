09c731292128673db18bfe7b467863fce5e737d1
griselleong <griselleong@gmail.com>
2021-05-06 22:21:00 -0400

    Update moreTableQueries.sql

--- moreTableQueries.sql
+++ moreTableQueries.sql
@@ -10,3 +10,9 @@ INNER JOIN Student s ON s.id = sch.student_id
 INNER JOIN Course c ON c.id = sch.course_id
 GROUP BY school_year, semester
 
+SELECT STUDENT_ID, count(*) 
+FROM StudentCourseHistory sch
+INNER JOIN Student s ON s.id = sch.student_id
+INNER JOIN Course c ON c.id = sch.course_id
+GROUP BY STUDENT_ID
+ORDER BY COUNT(*)
